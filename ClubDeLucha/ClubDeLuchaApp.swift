f7006eb6c6dae0646ef61859275fab8914053dec
Jose Catala <jfca68@gmail.com>
2024-10-28 10:03:01 +0100

    Initial Commit

---
+++ ClubDeLucha/ClubDeLuchaApp.swift
@@ -0,0 +1,20 @@
+//
+//  ClubDeLuchaApp.swift
+//  ClubDeLucha
+//
+//  Created by Jose on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClubDeLuchaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
